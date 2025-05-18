52afad2a771a784ab46b27ff206744b9b4c28a01
Robert Rothenberg <rrwo@cpan.org>
2025-05-18 17:51:19 +0100

    Fix typo

--- lib/Test/MixedScripts.pm
+++ lib/Test/MixedScripts.pm
@@ -37,7 +37,7 @@ This is a module to test that Perl code and other text files do not have potenti
 combinations.
 
 For example, the text for the domain names "E<0x043e>nE<0x0435>.example.com" and "one.example.com" look indistinguishable in many fonts,
-but the first one has Cyrillic letters.  If your software interactied with a service on the second domain, then someone
+but the first one has Cyrillic letters.  If your software interacted with a service on the second domain, then someone
 can operate a service on the first domain and attempt to fool developers into using their domain instead.
 
 This might be through a malicious patch submission, or even text from an email or web page that they have convinced a
